6fea236a1c86c7731d96d0641938ef1a655c0157
m0kitooo <mikolajklimuszko123@gmail.com>
2025-06-10 22:09:13 +0200

    fiexes

--- src/pages/proposals/ProposalDetails.jsx
+++ src/pages/proposals/ProposalDetails.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import {useParams, useNavigate, Link} from 'react-router-dom';
 import DialogBox from "../../components/DialogBox.jsx";
+import BasePageLayout from "../../components/BasePageLayout.jsx";
+import CORE_API_BASE_URL from "../../coreApiBaseUrl.jsx";
 
 const ProposalDetails = () => {
   const { id: proposalId } = useParams();
@@ -18,7 +20,7 @@ const ProposalDetails = () => {
 
       setError('');
       try {
-        const response = await fetch(`http://localhost:8080/api/proposal/details?proposalId=${proposalId}`, {
+        const response = await fetch(`${CORE_API_BASE_URL}/api/proposal/details?proposalId=${proposalId}`, {
           method: 'POST',
         });
 
@@ -42,7 +44,7 @@ const ProposalDetails = () => {
   const handleReject = async () => {
 
     try {
-      const response = await fetch(`http://localhost:8080/api/proposal/reject?proposalId=${proposalId}`, {
+      const response = await fetch(`${CORE_API_BASE_URL}/api/proposal/reject?proposalId=${proposalId}`, {
         method: 'POST',
       });
 
@@ -54,83 +56,80 @@ const ProposalDetails = () => {
     }
   };
 
-  if (dialogMessage !== null) {
-    return (
-      <DialogBox
-        message={dialogMessage}
-        returnLink='/proposal'
-      />
-    )
-  }
-
-
   return (
-    <div>
-      <h2>Proposal Details</h2>
+    <BasePageLayout>
+      <div>
+        <h2>Proposal Details</h2>
 
-      <form onSubmit={(e) => e.preventDefault()}>
-        <div>
-          <label htmlFor="title">Tytuł</label>
-          <input
-            id="title"
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            required
-            style={{ width: '100%', padding: '8px', boxSizing: 'border-box' }}
-          />
-          <label htmlFor="description">Opis</label>
-          <input
-            id="description"
-            type="text"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            style={{ width: '100%', padding: '8px', boxSizing: 'border-box' }}
-          />
-          <label htmlFor="proposedBy">Autor propozycji</label>
-          <input
-            id="proposedBy"
-            type="text"
-            value={proposedBy}
-            onChange={(e) => setProposedBy(e.target.value)}
-            style={{ width: '100%', padding: '8px', boxSizing: 'border-box' }}
-          />
-        </div>
-        <div style={{ marginTop: '16px' }}>
-          <button
-            type="button"
-            onClick={navigate("proposal/chooseOrganizer")}
-            style={{
-              padding: '8px 16px',
-              backgroundColor: '#4CAF50',
-              color: 'white',
-              border: 'none',
-              borderRadius: '4px',
-              cursor: 'pointer',
-              marginRight: '8px'
-            }}
-          >
-            Zaakceptuj
-          </button>
-          <button
-            type="button"
-            onClick={handleReject}
-            style={{
-              padding: '8px 16px',
-              backgroundColor: '#FF2400',
-              color: 'white',
-              border: 'none',
-              borderRadius: '4px',
-              cursor: 'pointer'
-            }}
-          >
-            Odrzuć
-          </button>
-        </div>
-      </form>
-
-      {error && <div style={{ color: 'red', marginTop: '20px' }}>{error}</div>}
-    </div>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <div>
+            <label htmlFor="title">Tytuł</label>
+            <input
+                id="title"
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                required
+                style={{ width: '100%', padding: '8px', boxSizing: 'border-box' }}
+            />
+            <label htmlFor="description">Opis</label>
+            <input
+                id="description"
+                type="text"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                style={{ width: '100%', padding: '8px', boxSizing: 'border-box' }}
+            />
+            <label htmlFor="proposedBy">Autor propozycji</label>
+            <input
+                id="proposedBy"
+                type="text"
+                value={proposedBy}
+                onChange={(e) => setProposedBy(e.target.value)}
+                style={{ width: '100%', padding: '8px', boxSizing: 'border-box' }}
+            />
+          </div>
+          <div style={{ marginTop: '16px' }}>
+            <button
+                type="button"
+                onClick={navigate("proposal/chooseOrganizer")}
+                style={{
+                  padding: '8px 16px',
+                  backgroundColor: '#4CAF50',
+                  color: 'white',
+                  border: 'none',
+                  borderRadius: '4px',
+                  cursor: 'pointer',
+                  marginRight: '8px'
+                }}
+            >
+              Zaakceptuj
+            </button>
+            <button
+                type="button"
+                onClick={handleReject}
+                style={{
+                  padding: '8px 16px',
+                  backgroundColor: '#FF2400',
+                  color: 'white',
+                  border: 'none',
+                  borderRadius: '4px',
+                  cursor: 'pointer'
+                }}
+            >
+              Odrzuć
+            </button>
+          </div>
+        </form>
+        {dialogMessage !== null ? (
+            <DialogBox
+                message={dialogMessage}
+                returnLink='/proposal'
+            />
+        ) : null}
+        {error && <div style={{ color: 'red', marginTop: '20px' }}>{error}</div>}
+      </div>
+    </BasePageLayout>
   );
 };
 
